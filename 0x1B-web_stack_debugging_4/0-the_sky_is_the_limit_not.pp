a4590e41fd89589c343858efb9904453b7a1b4b9
Z-ajamy <abdorahman0283@gmail.com>
2024-08-21 15:11:28 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,12 @@
-# Puppet script
-exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
