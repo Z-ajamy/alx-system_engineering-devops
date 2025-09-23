ac97b6d42e8264873dcebc5137b893373fe17a7f
Z-ajamy <abdorahman0283@gmail.com>
2025-09-23 03:52:52 +0300

    update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,8 @@
 # kills a process named killmenow
 
 exec{'kills_a_process_named_killmenow':
-    command  => '/bin/pkill killmenow'
-    path     => ['/bin', '/user/bin'],
+    command  => '/bin/pkill killmenow',
+    path     => ['/bin', '/usr/bin'],
     provider => 'shell',
+    onlyif   => '/bin/pgrep killmenow',
 }
