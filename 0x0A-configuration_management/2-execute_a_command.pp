194a40d221dd0602583ca362f864ed246dee2be2
Z-ajamy <abdorahman0283@gmail.com>
2025-09-23 03:19:59 +0300

    README

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# This manifest uses the pkill command to kill a process named 'killmenow'
-
-exec { 'kill-killmenow-process':
-  command => 'pkill killmenow',
-  path    => ['/usr/bin', '/bin'],
-}
