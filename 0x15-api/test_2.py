8c39e7e2c662c48216f4caad8e8dbd25859be03c
Z-ajamy <abdorahman0283@gmail.com>
2025-10-22 01:18:12 +0300

    ?

--- 0x15-api/test_2.py
+++ 0x15-api/test_2.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
 """
-Checks student output for returning info from REST API
+Check student .CSV output of user information
 """
 
+import csv
 import requests
 import sys
 
-users_url = "https://jsonplaceholder.typicode.com/users"
+users_url = "https://jsonplaceholder.typicode.com/users?id="
 todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def check_tasks(id):
-    """ Fetch user name, number of tasks """
+def user_info(id):
+    """ Check user information """
 
-    resp = requests.get(todos_url).json()
+    total_tasks = 0
+    response = requests.get(todos_url).json()
+    for i in response:
+        if i['userId'] == id:
+            total_tasks += 1
 
-    task = None
-    filename = '2.csv'
-    count = 0
-    with open(filename, 'r') as f:
-        output = f.read()
-        for i in resp:
-            if (i['completed'] and i['userId'] == id):
-                task = i['title']
-                count += 1
-                if output.find(task) != -1:
-                    print("Task {} in output: OK".format(count))
-                else:
-                    print("Task {} not in output".format(count))
+    response = requests.get(users_url + str(id)).json()
+    username = response[0]['username']
+
+    flag = 0
+    with open(str(id) + ".csv", 'r') as f:
+        for line in f:
+            if not line == '\n':
+                if not str(id) in line:
+                    print("User ID: Incorrect / ", end='')
+                    flag = 1
+                if not str(username) in line:
+                    print("Username: Incorrect")
+                    flag = 1
+
+    if flag == 0:
+        print("User ID and Username: OK")
 
 
 if __name__ == "__main__":
-    check_tasks(int(sys.argv[1]))
+    user_info(int(sys.argv[1]))
