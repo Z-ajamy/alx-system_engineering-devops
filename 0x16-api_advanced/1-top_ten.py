4f88bb7ce0944af941f1dfb8d4fbe64476243acb
Z-ajamy <abdorahman0283@gmail.com>
2025-12-10 16:52:10 +0200

    ?

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+
+def top_ten(subreddit: str) -> None:
+    endpoint = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "python:subreddit.subscriber.counter:v1.0 (by /u/abdo)"}
+    params = {"limit": 10}
+    try:
+        response = requests.get(endpoint, headers=headers, timeout=10, allow_redirects=False, params=params)
+        
+        if response.status_code != 200:
+            print (None)
+            return
+
+        data = response.json()
+        if not data:
+            print (None)
+            return
+
+
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            title = post.get("data", {}).get("title", "")
+            print(title)
+    except (requests.RequestException, ValueError):
+        print (None)
+        return
+
